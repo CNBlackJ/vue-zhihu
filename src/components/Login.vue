4dd5dc8558410910fd264fcad11e521af7698252
CNBlackJ <yes.heng@icloud.com>
2017-04-06 23:44:27 +0800

    init project

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    <h1>{{ users }}</h1>
+    <input type="text" v-model="username">
+    <button v-on:click="addUser">AddUser</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      username: 'a',
+      users: []
+    }
+  },
+  methods: {
+    addUser: function () {
+      this.users.push(this.username)
+    }
+  }
+}
+</script>
