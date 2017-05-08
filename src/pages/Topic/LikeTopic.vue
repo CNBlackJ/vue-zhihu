55f30512f5bba3d26649754dd17936298b8c0dc5
CNBlackJ <yes.heng@icloud.com>
2017-05-08 23:13:37 +0800

    add topic head style

--- src/pages/Topic/LikeTopic.vue
+++ src/pages/Topic/LikeTopic.vue
@@ -1,13 +1,85 @@
 <template lang="html">
   <div class="like-topic">
-    <p>Show like topic</p>
+    <div class="row like-topic-head">
+      <div class="col-md-12">
+        <span class="pull-left like-topic-title">
+          <i class="fa fa-list"></i>
+          已关注的话题动态
+        </span>
+        <div class="pull-right">
+          <a href="#" class="like-topic-count">共关注26个话题</a>
+        </div>
+      </div>
+    </div>
+    <ul class="topic-items">
+      <li v-for="(item, index) in topic_items"
+          :id="index"
+          class="topic-item"
+          v-on:click="isChoose(index)" v-bind:class="{ 'current-item': index === isActive }">
+        <a class="topic-item-link" :href="'#/topic#' + item">{{item}}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'like-topic',
+  data () {
+    return {
+      isActive: 0,
+      topic_items: ['阅读', '生活', '摄影', '运动', '西部世界', 'Vue.js']
+    }
+  },
+  methods: {
+    isChoose: function (index) {
+      this.isActive = index
+    }
+  }
 }
 </script>
 
 <style lang="css">
+  .like-topic-head {
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 8px;
+  }
+  .like-topic-title {
+    font-size: 14px;
+    font-weight: 700;
+    color: #666;
+  }
+  .like-topic-count {
+    font-size: 12px;
+    font-weight: 400;
+    color: #999;
+  }
+  ul li {
+    display: inline;
+  }
+  .topic-items {
+    padding: 18px 0 8px;
+  }
+  .topic-item {
+    float: left;
+    margin: 0 10px 10px 0;
+    list-style: none;
+  }
+  .topic-item a {
+    display: block;
+    padding: 2px 10px;
+    border-radius: 30px;
+    text-decoration: none;
+    border: 1px solid #daecf5;
+  }
+  .current-item a {
+    background: #259;
+    border-color: #259;
+    color: #fff;
+  }
+  li a:hover {
+    background: #259;
+    border-color: #259;
+    color: #fff;
+  }
 </style>
